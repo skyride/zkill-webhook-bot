3eef61b97c528e1970f8241b6cb61e395cd2dc4a
Adam Findlay <skylinerspeeder@gmail.com>
2018-03-20 01:17:25 +0000

    Fixed more parser bugs

--- core/parser.py
+++ core/parser.py
@@ -47,8 +47,8 @@ class Parser:
     def isk(self, package):
         return package['zkb']['totalValue'] > self.filters.get("isk")[0]
 
-    def attacker_type_id(self, package):
-        values = self.filters.get("attacker_type_id")
+    def attacker_type_id(self, package, values=None):
+        values = values or self.filters.get("attacker_type_id")
         return len(
             set(
                 self.attacker_property(package, "ship_type_id")
@@ -57,22 +57,22 @@ class Parser:
             )
         )
 
-    def attacker_group_id(self, package):
-        values = self.filters.get("attacker_group_id")
+    def attacker_group_id(self, package, values=None):
+        values = values or self.filters.get("attacker_group_id")
         return Group.objects.filter(
             types__id__in=self.attacker_property(package, "ship_type_id"),
             id__in=values
         ).exists()
 
-    def attacker_category_id(self, package):
-        values = self.filters.get("attacker_category_id")
+    def attacker_category_id(self, package, values=None):
+        values = values or self.filters.get("attacker_category_id")
         return Category.objects.filter(
             groups__types__id__in=self.attacker_property(package, "ship_type_id"),
             id__in=values
         ).exists()
 
-    def attacker_corporation_id(self, package):
-        values = self.filters.get("attacker_corporation_id")
+    def attacker_corporation_id(self, package, values=None):
+        values = values or self.filters.get("attacker_corporation_id")
         return len(
             set(
                 self.attacker_property(package, "alliance_id")
@@ -81,8 +81,8 @@ class Parser:
             )
         )
 
-    def attacker_alliance_id(self, package):
-        values = self.filters.get("attacker_alliance_id")
+    def attacker_alliance_id(self, package, values=None):
+        values = values or self.filters.get("attacker_alliance_id")
         return len(
             set(
                 self.attacker_property(package, "alliance_id")
@@ -91,30 +91,30 @@ class Parser:
             )
         )
 
-    def victim_type_id(self, package):
-        values = self.filters.get("victim_type_id")
+    def victim_type_id(self, package, values=None):
+        values = values or self.filters.get("victim_type_id")
         return package['killmail']['victim'].get("ship_type_id") in values
 
-    def victim_group_id(self, package):
-        values = self.filters.get("victim_group_id")
+    def victim_group_id(self, package, values=None):
+        values = values or self.filters.get("victim_group_id")
         return Type.objects.filter(
             id=package['killmail']['victim'].get("ship_type_id"),
             group_id__in=values
         ).exists()
 
-    def victim_category_id(self, package):
-        values = self.filters.get("victim_category_id")
+    def victim_category_id(self, package, values=None):
+        values = values or self.filters.get("victim_category_id")
         return Type.objects.filter(
             id=package['killmail']['victim'].get("ship_type_id"),
             group__category_id__in=values
         ).exists()
 
-    def victim_corporation_id(self, package):
-        values = self.filters.get("victim_corporation_id")
+    def victim_corporation_id(self, package, values=None):
+        values = values or self.filters.get("victim_corporation_id")
         return package['killmail']['victim'].get("corporation_id") in values
 
-    def victim_alliance_id(self, package):
-        values = self.filters.get("victim_alliance_id")
+    def victim_alliance_id(self, package, values=None):
+        values = values or self.filters.get("victim_alliance_id")
         return package['killmail']['victim'].get("alliance_id") in values
 
     def wspace(self, package):
@@ -127,19 +127,24 @@ class Parser:
 
     # Combined
     def type_id(self, package):
-        return self.attacker_type_id(package) or self.victim_type_id(package)
+        values = self.filters.get("type_id")
+        return self.attacker_type_id(package, values) or self.victim_type_id(package, values)
 
     def group_id(self, package):
-        return self.attacker_group_id(package) or self.victim_group_id(package)
+        values = self.filters.get("group_id")
+        return self.attacker_group_id(package, values) or self.victim_group_id(package, values)
 
     def category_id(self, package):
-        return self.attacker_category_id(package) or self.victim_category_id(package)
+        values = self.filters.get("category_id")
+        return self.attacker_category_id(package, values) or self.victim_category_id(package, values)
 
     def corporation_id(self, package):
-        return self.attacker_corporation_id(package) or self.victim_corporation_id(package)
+        values = self.filters.get("corporation_id")
+        return self.attacker_corporation_id(package, values) or self.victim_corporation_id(package, values)
 
     def alliance_id(self, package):
-        return self.attacker_alliance_id(package) or self.victim_alliance_id(package)
+        values = self.filters.get("alliance_id")
+        return self.attacker_alliance_id(package, values) or self.victim_alliance_id(package, values)
 
 
     # Send webhook
